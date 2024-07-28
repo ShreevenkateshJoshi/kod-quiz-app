e3707993b4ca9282ca0b96e9d7ca915ecb90c667
shreevenkateshjoshi <shreevenkateshjoshi@gmail.com>
2024-07-28 22:38:44 +0530

    list verison of quiz app

---
+++ src/Data.js
@@ -0,0 +1,42 @@
+export const data=[
+    {
+        question : '5 + 10',
+        option1 :'5',
+        option2 :'10',
+        option3 :'15',
+        option4 :'25',
+        ans :'3'
+    },
+    {
+        question : '2 * 20',
+        option1 :'20',
+        option2 :'40',
+        option3 :'60',
+        option4 :'80',
+        ans :'2'
+    },
+    {
+        question : '3 + 89',
+        option1 :'3',
+        option2 :'89',
+        option3 :'92',
+        option4 :'30',
+        ans :'3'
+    },
+    {
+        question : '2 + 10',
+        option1 :'12',
+        option2 :'10',
+        option3 :'45',
+        option4 :'25',
+        ans :'1'
+    },
+    {
+        question : '6 / 10',
+        option1 :'5',
+        option2 :'10',
+        option3 :'15',
+        option4 :'0.6',
+        ans :'4'
+    }
+]
